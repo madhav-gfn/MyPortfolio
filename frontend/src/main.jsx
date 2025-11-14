dbbea38f55d501c65dbb65d904ba4047dbdb1382
madhav_gfn <madmishra72@gmail.com>
2025-11-15 00:58:43 +0530

    nice commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,28 +1,10 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
-import './index.css';
-
-const theme = extendTheme({
-  fonts: { heading: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif', body: 'Inter, system-ui, Avenir, Helvetica, Arial, sans-serif' },
-  initialColorMode: 'light',
-  useSystemColorMode: false,
-});
-
-const queryClient = new QueryClient();
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ChakraProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
