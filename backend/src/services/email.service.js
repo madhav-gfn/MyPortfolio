be1d9d61d9c2b841f6903bd4674bc235b456ff9c
madhav_gfn <madmishra72@gmail.com>
2026-03-11 00:08:45 +0530

    fixing done

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -7,7 +7,9 @@ import nodemailer from 'nodemailer';
 import config from '../config/index.js';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // Use SSL
     auth: {
         user: config.email.user,
         pass: config.email.pass,
